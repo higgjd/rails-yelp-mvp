a04dc42d0ded62ea5731fc4211059b91db7f34af
Jenny Higgins <higgjd@gmail.com>
2022-04-26 21:31:46 +0100

    created restaurant and reviews models and controllers, as well as index and new actions

--- db/migrate/20220426182842_create_reviews.rb
+++ db/migrate/20220426182842_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
     create_table :reviews do |t|
       t.integer :rating
       t.text :content
-      t.reference :restaurant
+      t.references :restaurant
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning seeds...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+50.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "Restaurant #{restaurant.id} is created"
+end
+
+puts 'Done!'
